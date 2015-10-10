4d194018de04277d89c0486f123278f1fb09a91b
MoonJongRak <MoonJR@MoonJongRakui-MacBook-Pro.local>
2015-10-10 21:51:31 +0900

    facebook 로그인 추가

--- speecherServer/routes/index.js
+++ speecherServer/routes/index.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var session = require('express-session');
 
+
+var facebook = require('./snsLogin');
 /* GET home page. */
 
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+//세션 사용
+router.use(session({
+  secret: 'Soma',
+  resave: false,
+  saveUninitialized: true
+}));
+
+router.get('/', function (req, res) {
+  res.render('index', {title: 'Express'});
 });
+
+router.get('/login/facebook', facebook.facebookLogin)
+
 
 module.exports = router;
---
+++ speecherServer/routes/snsLogin.js
@@ -0,0 +1,56 @@
+/**
+ * Created by MoonJR on 2015. 10. 10..
+ */
+
+//mongoDB Connection Pool
+var mongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://somalunak.cafe24.com:27017/soma';
+var mongoDB = null; //mongoDB로 부터 collection 생성해서 사용하면 된다.
+
+mongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log(url + "에 연결할 수 없습니다.")
+  } else {
+    console.log(url + "에 연결 하였습니다.");
+    mongoDB = db;
+  }
+});
+
+//mongoDB Connection Pool
+
+
+var http = require('https');
+exports.facebookLogin = function (req, res) {
+
+  var facebookToken = req.query.token;
+
+  getFaceBookData(facebookToken, function (response) {
+    var collection = mongoDB.collection('user');
+
+    collection.insert(response, function (err, result) {
+      res.send(response);
+    })
+
+  });
+
+};
+
+
+function getFaceBookData(token, varFunction) {
+  url = 'https://graph.facebook.com/me?fields=email,name&access_token=' + token;
+  http.get(url, function (res) {
+    var body = '';
+
+    res.on('data', function (chunk) {
+      body += chunk;
+    });
+
+    res.on('end', function () {
+      var fbResponse = JSON.parse(body);
+      varFunction(fbResponse);
+    });
+  }).on('error', function (e) {
+    console.log("Got an error: ", e);
+  });
+
+}
