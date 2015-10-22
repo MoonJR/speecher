3163cffeb942f35110192008039a071672c496e7
MoonJongRak <mox7050@gmail.com>
2015-10-23 00:09:11 +0900

    대본 불러오기 완료

--- speecherServer/models/dbCollection.js
+++ speecherServer/models/dbCollection.js
@@ -4,23 +4,16 @@
 //mongoDB Connection Pool
 var mongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://somalunak.cafe24.com:27017/soma';
-var mongoDB = null; //mongoDB로 부터 collection 생성해서 사용하면 된다.
-
-var scriptCollection;
-var paragraphCollection;
-var morphemeCollection;
-
 
 mongoClient.connect(url, function (err, db) {
   if (err) {
     console.log(url + "에 연결할 수 없습니다.")
   } else {
     console.log(url + "에 연결 하였습니다.");
-    mongoDB = db;
 
-    exports.scriptCollection = mongoDB.collection('script');
-    exports.paragraphCollection = mongoDB.collection('paragraph');
-    exports.morphemeCollection = mongoDB.collection('morpheme');
+    exports.scriptCollection = db.collection('script');
+    exports.paragraphCollection = db.collection('paragraph');
+    exports.morphemeCollection = db.collection('morpheme');
   }
 });
 
