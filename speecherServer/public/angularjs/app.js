3601bab58cc95878bdfc09de5ffeb54f664405cb
jaisonoh <jaisonoh@naver.com>
2015-10-26 15:28:56 +0900

    login using angular service component

--- speecherServer/public/angularjs/app.js
+++ speecherServer/public/angularjs/app.js
@@ -3,65 +3,64 @@
 angular.module('myApp', [
   'ngRoute',
   'ngMaterial',
+  'ngCookies',
   'ngFacebook',
   'angular-svg-round-progress',
   'googleplus'
 ])
-  .config(function($routeProvider, $locationProvider, $facebookProvider, GooglePlusProvider) {
-    $routeProvider
-      .when('/login', {
-        templateUrl: '/partials/login',
-        controller: 'loginCtrl'
-      })
-      .when('/', {
-        templateUrl: '/partials/index',
-        controller: 'indexCtrl'
-      }).
-      when('/write', {
-        templateUrl: '/partials/write',
-        controller: 'writeCtrl',
-      }).
-      otherwise({
-        redirectTo: '/login'
+    .config(function ($routeProvider, $locationProvider, $facebookProvider, GooglePlusProvider) {
+      $routeProvider
+          .when('/', {
+            templateUrl: '/partials/index',
+            controller: 'indexCtrl'
+          })
+          .when('/login', {
+            templateUrl: '/partials/login',
+            controller: 'loginCtrl'
+          })
+          .when('/write', {
+            templateUrl: '/partials/write',
+            controller: 'writeCtrl',
+          })
+          .otherwise({
+            redirectTo: '/'
+          });
+
+      $facebookProvider.setAppId('1386879811618975');
+
+      $facebookProvider.setVersion("v2.5");
+
+      $facebookProvider.setPermissions('email');
+
+      GooglePlusProvider.init({
+        clientId: '280233748418-v4s1qjeknmb0lp6to142bblv4ab0rjcv.apps.googleusercontent.com',
+        apiKey: 'AIzaSyDcLpimE6IcAL-jSDPMuWKB1i8VnxpN9Zo'
       });
 
-    $facebookProvider.setAppId('1386879811618975');
+      GooglePlusProvider.enableServerSide({});
 
-    $facebookProvider.setVersion("v2.5");
+      $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+      });
 
-    $facebookProvider.setPermissions('email');
 
-    GooglePlusProvider.init({
-      clientId: '280233748418-v4s1qjeknmb0lp6to142bblv4ab0rjcv.apps.googleusercontent.com',
-      apiKey: 'AIzaSyDcLpimE6IcAL-jSDPMuWKB1i8VnxpN9Zo'
+    })
+    .run(function ($rootScope) {
+      // Load the facebook SDK asynchronously
+      (function () {
+
+        // Get the first script element, which we'll use to find the parent node
+        var firstScriptElement = document.getElementsByTagName('script')[0];
+
+        // Create a new script element and set its id
+        var facebookJS = document.createElement('script');
+        facebookJS.id = 'facebook-jssdk';
+
+        // Set the new script's source to the source of the Facebook JS SDK
+        facebookJS.src = '//connect.facebook.net/en_US/all.js';
+
+        // Insert the Facebook JS SDK into the DOM
+        firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
+      }());
     });
-
-    GooglePlusProvider.enableServerSide({
-
-    });
-
-    $locationProvider.html5Mode({
-      enabled: true,
-      requireBase: false
-    });
-
-
-  })
-  .run(function($rootScope) {
-    // Load the facebook SDK asynchronously
-    (function(){
-
-      // Get the first script element, which we'll use to find the parent node
-      var firstScriptElement = document.getElementsByTagName('script')[0];
-
-      // Create a new script element and set its id
-      var facebookJS = document.createElement('script');
-      facebookJS.id = 'facebook-jssdk';
-
-      // Set the new script's source to the source of the Facebook JS SDK
-      facebookJS.src = '//connect.facebook.net/en_US/all.js';
-
-      // Insert the Facebook JS SDK into the DOM
-      firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
-    }());
-  });
--- speecherServer/public/angularjs/controllers/loginCtrl.js
+++ speecherServer/public/angularjs/controllers/loginCtrl.js
@@ -1,18 +1,17 @@
 'use strict';
 
 angular.module('myApp')
-    .controller('loginCtrl', function($scope, $facebook, GooglePlus, $http, $location){
+    .controller('loginCtrl', function($scope, $facebook, GooglePlus, loginService, $location){
 
       // init login status
-      (function initController() {
-        $scope.logout;
-      })();
 
       // APIs
       $scope.loginStatus = null;
       $scope.facebookLogin = facebookLogin;
       $scope.googleLogin = googleLogin;
       $scope.logout = logout;
+
+      logout();
 
       function facebookLogin() {
         $facebook.login().then(
@@ -36,39 +35,57 @@ angular.module('myApp')
           GooglePlus.logout();
         }
         $scope.loginStatus = null;
+        loginService.clearCredentials();
       }
 
       // private functions
       function successFacebookLogin () {
         $facebook.getLoginStatus().then(
             function (response) {
-              var token = response.authResponse.accessToken;
-              $scope.loginStatus = 'facebook';
-              sendToken(token, $scope.loginStatus);
+              if(response.authResponse) {
+                console.log(response);
+                var token = response.authResponse.accessToken;
+                $scope.loginStatus = 'facebook';
+                loginToServer($scope.loginStatus, token)
+              }
             },
             errorHandler('Error: getLoginStatus')
         );
       }
 
       function successGoogleLogin (response) {
+        console.log(response);
         var token = response.id_token;
         $scope.loginStatus = 'google';
         sendToken(token, $scope.loginStatus);
       }
 
-      function sendToken (token, url) {
-        $http({
-          method: 'GET',
-          url: '/login/' + url,
-          params: {token: token}
-        }).then(
-            function (response) {
-              $location.path('/index');
-              console.log(response.data);
-            },
-            errorHandler('Error: sendToken')
-        );
+      function loginToServer(url, token) {
+        return loginService.sendCredentials(url, token, function(response) {
+          if(response.data.success) {
+            loginService.setCredentials(response.data.response.email);
+          }
+        });
       }
+
+
+      //function sendToken (token, url) {
+      //  $http({
+      //    method: 'GET',
+      //    url: '/login/' + url,
+      //    params: {token: token}
+      //  }).then(
+      //      function (response) {
+      //        console.log(response);
+      //        if(response.status === 200) {
+      //          loginService.setCredentials(response.data.email);
+      //          $location.path('/');
+      //          console.log(response.data);
+      //        }
+      //      },
+      //      errorHandler('Error: sendToken')
+      //  );
+      //}
 
       function errorHandler (error) {
         return { success: false, message: error };
