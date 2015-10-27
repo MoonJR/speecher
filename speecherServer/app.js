f09c6b3532c8ef419e452e37500f99606070bafd
appulele <appulele@gmail.com>
2015-10-27 22:38:53 +0900

    login  npm 설치시,  node_modules 내부는 public 으로 못불러오는것 수정

--- speecherServer/app.js
+++ speecherServer/app.js
@@ -38,6 +38,7 @@ app.use(session({
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', routes);
 app.use('/users', users);
