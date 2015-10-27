dc24f8e4f1e7a4f5d412588c301b389aaeec4409
appulele <appulele@gmail.com>
2015-10-28 02:58:41 +0900

    base test angular

--- speecherServer/public/angularjs/app.js
+++ speecherServer/public/angularjs/app.js
@@ -11,26 +11,34 @@ angular.module('myApp', [
 ])
     .config(function ($routeProvider, $locationProvider, $facebookProvider, GooglePlusProvider) {
       $routeProvider
-          .when('/', {
-            templateUrl: '/partials/index',
-            controller: 'indexCtrl'
-          })
-          .when('/login', {
-            templateUrl: '/partials/login',
-            controller: 'loginController',
-            controllerAs: 'vm'
-          })
-          .when('/write', {
-            templateUrl: '/partials/write',
-            controller: 'writeCtrl'
-          })
-          .when('/choice/:scriptId', {
-            templateUrl: '/partials/choice',
-            controller: 'choiceCtrl',
-          })
-          .otherwise({
-            redirectTo: '/'
-          });
+        .when('/', {
+          templateUrl: '/partials/index',
+          controller: 'indexCtrl'
+        })
+        .when('/login', {
+          templateUrl: '/partials/login',
+          controller: 'loginController',
+          controllerAs: 'vm'
+        })
+        .when('/write', {
+          templateUrl: '/partials/write',
+          controller: 'writeCtrl'
+        })
+        .when('/choice/:scriptId', {
+          templateUrl: function(params){
+            return '/partials/choice/'+params.scriptId;
+          },
+          controller: 'choiceCtrl',
+        })
+        .when('/test', {
+          templateUrl: function(params){
+            return '/partials/test';
+          },
+          controller: 'testCtrl',
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
 
       $facebookProvider.setAppId('1386879811618975');
 
@@ -52,40 +60,41 @@ angular.module('myApp', [
       });
 
 
-    })
-    .run(function ($rootScope, $location, $cookieStore, $http) {
-      // Load the facebook SDK asynchronously
-
-      (function () {
-        // Get the first script element, which we'll use to find the parent node
-        var firstScriptElement = document.getElementsByTagName('script')[0];
-
-        // Create a new script element and set its id
-        var facebookJS = document.createElement('script');
-        facebookJS.id = 'facebook-jssdk';
-
-        // Set the new script's source to the source of the Facebook JS SDK
-        facebookJS.src = '//connect.facebook.net/en_US/all.js';
-
-        // Insert the Facebook JS SDK into the DOM
-        firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
-      }());
-
-      // keep user logged in after page refresh
-
-      $rootScope.globals = $cookieStore.get('globals') || {};
-      if ($rootScope.globals.currentUser) {
-        $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
-      }
-
-      $rootScope.$on('$locationChangeStart', function (event, next, current) {
-        // redirect to login page if not logged in and trying to access a restricted page
-        //var restrictedPage = $.inArray($location.path(), ['/login']) === -1;
-
-        var loggedIn = $rootScope.globals.currentUser;
-        if (!loggedIn) {
-          $location.path('/login');
-        }
-      });
-
     });
+
+    //.run(function ($rootScope, $location, $cookieStore, $http) {
+    //  // Load the facebook SDK asynchronously
+    //
+    //  (function () {
+    //    // Get the first script element, which we'll use to find the parent node
+    //    var firstScriptElement = document.getElementsByTagName('script')[0];
+    //
+    //    // Create a new script element and set its id
+    //    var facebookJS = document.createElement('script');
+    //    facebookJS.id = 'facebook-jssdk';
+    //
+    //    // Set the new script's source to the source of the Facebook JS SDK
+    //    facebookJS.src = '//connect.facebook.net/en_US/all.js';
+    //
+    //    // Insert the Facebook JS SDK into the DOM
+    //    firstScriptElement.parentNode.insertBefore(facebookJS, firstScriptElement);
+    //  }());
+    //
+    //  // keep user logged in after page refresh
+    //
+    //  $rootScope.globals = $cookieStore.get('globals') || {};
+    //  if ($rootScope.globals.currentUser) {
+    //    $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata; // jshint ignore:line
+    //  }
+    //
+    //  $rootScope.$on('$locationChangeStart', function (event, next, current) {
+    //    // redirect to login page if not logged in and trying to access a restricted page
+    //    //var restrictedPage = $.inArray($location.path(), ['/login']) === -1;
+    //
+    //    var loggedIn = $rootScope.globals.currentUser;
+    //    if (!loggedIn) {
+    //      $location.path('/login');
+    //    }
+    //  });
+    //
+    //});
