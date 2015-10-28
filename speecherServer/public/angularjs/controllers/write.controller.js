f1deeaa31ac669825560c1db2f554d09799363b8
jaisonoh <jaisonoh@naver.com>
2015-10-28 16:38:07 +0900

    test code for script load

--- speecherServer/public/angularjs/controllers/write.controller.js
+++ speecherServer/public/angularjs/controllers/write.controller.js
@@ -5,14 +5,35 @@
       .module('myApp')
       .controller('writeController', writeController);
 
-  writeController.$inject = ['$scope'];
-  function writeController($scope) {
+  writeController.$inject = ['$rootScope', '$scope'];
+  function writeController($rootScope, $scope) {
+
+    //var vm = this;
+    //
+    //vm.title = null;
+    //vm.content = null;
+    //vm.readFile = readFile;
+    //
+    //function readFile(element) {
+    //  console.log('files:', element.files);
+    //  var reader = new FileReader();
+    //  //var file = onChangeEvent.target.files[0];
+    //
+    //  reader.onload = function(element) {
+    //    $rootScope.$apply(function() {
+    //      vm.title = file.name;
+    //      vm.content = onLoadEvent.target.result;
+    //    });
+    //  };
+    //  //
+    //  //reader.readAsText(file);
+    //  console.log(vm.title);
+    //  console.log(vm.content);
+    //}
 
     $scope.readFile = function($file){
       $scope.title = $file.title;
-      console.log($scope.title);
       $scope.content = $file.content;
-      console.log($scope.content);
     };
   }
 })();
