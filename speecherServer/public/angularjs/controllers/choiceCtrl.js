52df36a8ee7a8a18602ce0a95b3e5262864d19ee
appulele <appulele@gmail.com>
2015-10-30 00:49:14 +0900

    choice test mode 에서  timer 설정

--- speecherServer/public/angularjs/controllers/choiceCtrl.js
+++ speecherServer/public/angularjs/controllers/choiceCtrl.js
@@ -6,10 +6,18 @@ angular
   .module('myApp')
   .controller('choiceCtrl', choiceController);
 
-choiceController.$inject = ['$rootScope', 'choiceService', '$location'];
+choiceController.$inject = ['$scope', '$rootScope', 'choiceService', '$location'];
 
-function choiceController($rootScope, choiceService, $location) {
+function choiceController($scope, $rootScope, choiceService, $location) {
   $rootScope.test = choiceService;
+
+  $scope.addTimer = function($min){
+    choiceService.timer_value += $min;
+  }
+  $scope.setTimer = function($min){
+    choiceService.timer_value = $min;
+  }
+
 
 
 };
--- speecherServer/public/angularjs/services/choice.service.js
+++ speecherServer/public/angularjs/services/choice.service.js
@@ -10,12 +10,17 @@
     var test = {};
     test = {
       script_id: '0',
+      timer_status:true,
+      timer_value:5,
       script_title: 'Example Title',
       point: '84',
       script_content: " I'll be in your neighborhood doing errands",
-      reg_date: 2015/11/19
+      reg_date: 2015/11/19,
     }
     return test;
   }
+
+
+
 
 })();
