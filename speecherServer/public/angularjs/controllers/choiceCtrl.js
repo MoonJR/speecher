3321f9ac15306691d21b88144600e65980e61b84
appulele <appulele@gmail.com>
2015-11-01 01:23:12 +0900

    conflict backup --not read it

--- speecherServer/public/angularjs/controllers/choiceCtrl.js
+++ speecherServer/public/angularjs/controllers/choiceCtrl.js
@@ -13,30 +13,32 @@ function choiceController($scope, $rootScope, choiceService, $location) {
   $rootScope.test = choiceService;
 
 
-
-  //var timer;
-  //$scope.counter = 0;
-  //$scope.stopCounter = function() {
-  //  $timeout.cancel(timer);
-  //};
-  //var updateCounter = function() {
-  //  $scope.counter++;
-  //  timer = $timeout(updateCounter, 1000);
-  //};
+  var timer;
+  $scope.counter = 0;
+  $scope.stopCounter = function() {
+    $timeout.cancel(timer);
+  };
+  $scope.startCounter = function() {
+    updateCounter
+  };
+  var updateCounter = function() {
+    $scope.counter++;
+    timer = $timeout(updateCounter, 1000);
+  };
   //updateCounter();
-  $rootScope.setTimer = setTimer;
-
-  function setTimer($min,$type){
-    if($rootScope.test.timer_value < 1){
-      $rootScope.test.timer_value = 1;
-    } else if($type == 'add'){
-      $rootScope.test.timer_value += $min;
-    } else if($type == 'set'){
-      $rootScope.test.timer_value = $min;
-    }
-    $rootScope.test.timer_seconds = $rootScope.test.timer_value * 60;
-    $rootScope.test.timer_percent = $rootScope.test.current_seconds/test.timer_seconds;
-  }
+  //$rootScope.setTimer = setTimer;
+  //
+  //function setTimer($min,$type){
+  //  if($rootScope.test.timer_value < 1){
+  //    $rootScope.test.timer_value = 1;
+  //  } else if($type == 'add'){
+  //    $rootScope.test.timer_value += $min;
+  //  } else if($type == 'set'){
+  //    $rootScope.test.timer_value = $min;
+  //  }
+  //  $rootScope.test.timer_seconds = $rootScope.test.timer_value * 60;
+  //  $rootScope.test.timer_percent = $rootScope.test.current_seconds/test.timer_seconds;
+  //}
 
 
 };
