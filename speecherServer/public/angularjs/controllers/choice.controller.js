9560069256c77c42fd557802fc0df12be3b8ba8c
appulele <appulele@gmail.com>
2015-11-04 18:37:07 +0900

    타이머, 결과페이지기본폼

--- speecherServer/public/angularjs/controllers/choice.controller.js
+++ speecherServer/public/angularjs/controllers/choice.controller.js
@@ -32,17 +32,17 @@ function choiceController($scope, $rootScope, $cookieStore, choiceService, $loca
 
 
 
-  $scope.stopCounter = function() {
-    $timeout.cancel(timer);
-  };
-  $scope.startCounter = function() {
-    updateCounter
-  };
-  var updateCounter = function() {
-    $scope.counter++;
-    timer = $timeout(updateCounter, 1000);
-
-  };
+  //$scope.stopCounter = function() {
+  //  $timeout.cancel(timer);
+  //};
+  //$scope.startCounter = function() {
+  //  updateCounter
+  //};
+  //var updateCounter = function() {
+  //  $scope.counter++;
+  //  timer = $timeout(updateCounter, 1000);
+  //
+  //};
 
 
   // Timer Setting 값을 저장하고, 테스트 페이지로 이동
--- speecherServer/public/angularjs/services/choice.service.js
+++ speecherServer/public/angularjs/services/choice.service.js
@@ -5,28 +5,28 @@
     .module('myApp')
     .factory('choiceService', choiceService);
 
-  choiceService.$inject = ['$http', '$cookieStore', '$rootScope'];
+  choiceService.$inject = ['$http', '$cookieStore', '$rootScope','$location'];
   //가장 최근에 선택된 test 글로벌 값을 관리한다
-  function choiceService($http, $cookieStore, $rootScope) {
+  function choiceService($http, $cookieStore, $rootScope, $location) {
     var test = {};
     //function
     test.startTest = startTest;
-    test.addTimer = addTimer;
     test.saveItem = saveItem;
     test.setTimer = setTimer;
+    test.counter = 5;
+    test.timer_status = true;
+    test.timer_seconds = 300;
+    test.current_seconds = 0;
+    test.timer_percent = 0;
+    test.status = "WAIT";
+
+    //init value
+
 
     return test;
 
 
-    function addTimer(min){
-      if(test.timer_value < 0){
-        test.timer_value = 0;
-      } else{
-        test.timer_value += min;
-      }
-      test.timer_seconds = test.timer_value * 60;
-      test.timer_percent = test.current_seconds/test.timer_seconds;
-    }
+
 
     //  indexPage 에서 테스트 버튼 클릭시 관련 스크립트의 데이터를 rootScope 의 test에서 쓸 수 있도록 저장한다
     function saveItem(item){
@@ -37,21 +37,21 @@
           test[key] = obj;
         }
       }
-      $cookieStore.put('test', test);
-      var cookie = $cookieStore.get('test');
-      console.log("cookie:"+cookie);
+      $cookieStore.put("test", test);
+      var cookie = $cookieStore.get("test");
+      console.log("cookie:"+cookie["script_id"]);
     }
 
     function setTimer($min,$type){
-      if($rootScope.test.counter < 1){
-        $rootScope.test.counter = 1;
+      if(test.counter < 1){
+        test.counter = 1;
       } else if($type == 'add'){
-        $rootScope.test.counter += $min;
+        test.counter += $min;
       } else if($type == 'set'){
-        $rootScope.test.counter = $min;
+        test.counter = $min;
       }
-      $rootScope.test.timer_seconds = $rootScope.test.timer_value * 60;
-      $rootScope.test.timer_percent = $rootScope.test.current_seconds/$rootScope.test.timer_seconds;
+      test.timer_seconds = test.counter * 60;
+      test.timer_percent = test.current_seconds/test.timer_seconds;
       //$cookieStore.put('test', $rootScope.test);
     }
 
