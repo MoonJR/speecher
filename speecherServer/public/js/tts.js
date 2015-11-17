101cb333bab0518bd4e21a7d8cb3db73acb35555
appulele <appulele@gmail.com>
2015-11-18 05:33:16 +0900

    CLose modal -> close TTS

--- speecherServer/public/js/tts.js
+++ speecherServer/public/js/tts.js
@@ -11,7 +11,7 @@ $(document).on("click", ".play", function() {
   msg.voice = voices[8]; // Note: some voices don't support altering params
   msg.voiceURI = 'native';
   msg.volume = 1; // 0 to 1
-  msg.rate = 5; // 0.1 to 10
+  msg.rate = 10; // 0.1 to 10
   msg.pitch = 0; //0 to 2
   msg.text = value;
   msg.lang = 'en';
@@ -44,7 +44,7 @@ $(document).on("click", ".play-tts", function() {
     msg.voice = voices[83]; // Note: some voices don't support altering params
     msg.voiceURI = 'native';
     msg.volume = 1; // 0 to 1
-    msg.rate = 5; // 0.1 to 10
+    msg.rate = 10; // 0.1 to 10
     msg.pitch = 0; //0 to 2
     msg.text = value;
     msg.lang = 'en';
@@ -57,11 +57,12 @@ $(document).on("click", ".play-tts", function() {
   }
 
 
-
 });
 
+$(document).on("click", ".close", function() {
+  window.speechSynthesis.cancel();
 
-
+});
 
 
 
