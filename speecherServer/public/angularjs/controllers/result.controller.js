3bcd289c012e2f65378fc02f26f8921af4a4d780
appulele <appulele@gmail.com>
2015-11-19 03:54:24 +0900

    RESULT page word modal fix

--- speecherServer/public/angularjs/controllers/result.controller.js
+++ speecherServer/public/angularjs/controllers/result.controller.js
@@ -123,13 +123,20 @@
         function (response) {
           console.log(response);
           if(response.data.success) {
-            console.log(response.data.success);
-            var pros = response.data.result.pronunciation.all;
-            if(pros == undefined) pros = response.data.result.pronunciation;
+            var pros = null;
+            var title = null;
+            try{
+              pros = response.data.result.pronunciation.all;
+              if(pros == undefined) pros = response.data.result.pronunciation;
+              title = word +"("+pros+")";
+            }catch(e){
+              title = word;
+            }
+
             $mdDialog.show(
               $mdDialog.alert()
                 .clickOutsideToClose(true)
-                .title(word +"("+pros+")")
+                .title(title)
                 .content(
                 '<button class="play btn right md-primary md-button" value="'+word+'"><img src="../images/icon_play.png" style="margin:9px"></button> <input type="text" class="repeat  hidden" value="3" maxlength="1"/>')
                 .ok('닫기')
