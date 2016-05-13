ae94f4d68e6e584bcf34d6d74768317e5d8556d7
minkikim89 <minkikim89@gmail.com>
2016-05-13 09:40:15 +0900

    Update dbConn.js

--- speecherServer/models/dbConn.js
+++ speecherServer/models/dbConn.js
@@ -4,10 +4,12 @@
 
 var mongodb = require('mongodb');
 
+var ip;
+var port;
 var server = new mongodb.Server(
-  'somalunak.cafe24.com',
-  27017,
+  ip,
+  port,
   {auto_reconnect: true, poolSize: 100});
-var db = new mongodb.Db('soma', server);
+var db = new mongodb.Db('speecher', server);
 
 module.exports = db;
